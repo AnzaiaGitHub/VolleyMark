fc6ac9c4138c08581a8143aa2557e793e6aea7b2
Julian Pedroza <julianpedrozagarcia@gmail.com>
2022-08-13 16:25:31 -0500

    fixed the set options, set win condition

--- VolleyMark.js
+++ VolleyMark.js
@@ -1,9 +1,9 @@
 //Variables
 const Modal = document.getElementById("modal");
 const modalContent = document.getElementById("modalContent");
-var Game = null;
-var TeamA,TeamB, maxSetPoints;
-var RenderTeamA = {
+let Game = null;
+let TeamA,TeamB, maxSetPoints;
+let RenderTeamA = {
     Name: document.querySelector("#teamNameA"),
     SetsWon: document.querySelector("#setScoreA"),
     Score: document.querySelector("#scoreMarkA"),
@@ -17,7 +17,7 @@ var RenderTeamA = {
     document.querySelector("#timeOutA2")],
     Service: document.querySelector("#serviceA")
 };
-var RenderTeamB = {
+let RenderTeamB = {
     Name: document.querySelector("#teamNameB"),
     SetsWon: document.querySelector("#setScoreB"),
     Score: document.querySelector("#scoreMarkB"),
@@ -342,54 +342,110 @@ function subtractSetWonTeam(team){
 
 function closeModal(){
     Modal.style.display = "none";
+    modalContent.innerHTML = "";
 }
 
 function editOptions(){
-    let render =`
-    <form>
-        <label for="maxPoints">
-            Puntos máximos por set
-            <input id="maxPoints" type="text" value="${maxSetPoints.Points}" placeholder="25">
-        </label>
-        <label for="pointsDiff">
-            Gana por diferencia de 2 puntos
-            <input name="winCondition" id="pointsDiff" type="radio">
-        </label>
-        <label for="goldenPoint" id="goldenPointLabel">
-            Gana el primero con ${maxSetPoints.Points} puntos
-            <input name="winCondition" id="goldenPoint" type="radio">
-        </label>
-        <div class="form-end">
-            <button class="btn btn-standard" onclick="closeModal()">Cancelar</button>
-            <button class="btn btn-standard" id="saveInfo">Guardar</button>
-        </div>
-    </form>
-    `;
-    setModalContent(render);
-    let pDiff = document.getElementById("pointsDiff");
-    let gPoint = document.getElementById("goldenPoint");
-    let mPoints = document.getElementById("maxPoints");
-    mPoints.addEventListener("input",()=>{
-        document.getElementById("goldenPointLabel").innerHTML=`Gana el primero con ${mPoints.value} puntos
-        <input name="winCondition" id="goldenPoint" type="radio">`;
+    let maxPoints_input = document.createElement("input");
+    maxPoints_input = setAttributes(maxPoints_input,[
+        ["id","maxPoints"],
+        ["type","text"],
+        ["placeholder",25],
+        ["value",maxSetPoints.Points]
+    ]);
+
+    let maxPoints_label = document.createElement("label");
+    maxPoints_label.innerHTML = "Puntos máximos por Set";
+    maxPoints_label.appendChild(maxPoints_input);
+    maxPoints_label = setAttributes(maxPoints_label,[["for",maxPoints_input.id]]);
+
+    let pDiff_input = document.createElement("input");
+    pDiff_input = setAttributes(pDiff_input,[
+        ["id","pointsDiff"],
+        ["type","radio"],
+        ["name","winCondition"]
+    ]);
+    let pDiff_label = document.createElement("label");
+    pDiff_label.innerHTML = "Gana por diferencia de 2 puntos";
+    pDiff_label.appendChild(pDiff_input);
+    pDiff_label = setAttributes(pDiff_label,[["for",pDiff_input.id]]);
+
+    let gPoint_input = document.createElement("input");
+    gPoint_input = setAttributes(gPoint_input,[
+        ["id","goldenPoint"],
+        ["type","radio"],
+        ["name","winCondition"]
+    ]);
+    let gPoint_label = document.createElement("label");
+    gPoint_label.innerHTML = `Gana el primero con ${maxSetPoints.Points} puntos`;
+    gPoint_label.appendChild(gPoint_input);
+    gPoint_label = setAttributes(gPoint_label,[
+        ["for",gPoint_input.id],
+        ["id","goldenPoint_label"],
+    ]);
+
+    let formEnd = document.createElement("div");
+    formEnd.classList.add("form-end");
+    let cancelButton = document.createElement("button");
+    cancelButton.innerHTML = "Cancelar";
+    cancelButton = setAttributes(cancelButton,[
+        ["class","btn btn-padding btn-secondary"],
+        ["onclick","closeModal()"]
+    ]);
+
+    let saveButton = document.createElement("button");
+    saveButton.innerHTML = "Guardar";
+    saveButton = setAttributes(saveButton,[
+        ["id","saveInfo"],
+        ["class","btn btn-standard btn-padding"]
+    ]);
+
+    maxPoints_input.addEventListener("input",()=>{
+        gPoint_label.innerHTML=`Gana el primero con ${maxPoints_input.value} puntos`;
+        gPoint_label.appendChild(gPoint_input);
     });
     if(maxSetPoints.diffPoints)
-        pDiff.setAttribute("checked",true);
+        pDiff_input.setAttribute("checked",true);
     else
-        gPoint.setAttribute("checked",true);
-    document.getElementById("saveInfo").addEventListener("click",()=>{
-        maxSetPoints.Points=isNaN(parseInt(mPoints.value))?maxSetPoints.Points:parseInt(mPoints.value);
-        maxSetPoints.diffPoints = pDiff.checked;
-        maxSetPoints.firstReachMax = gPoint.checked;
+        gPoint_input.setAttribute("checked",true);
+
+    saveButton.addEventListener("click",()=>{
+        maxSetPoints.Points=isNaN(parseInt(maxPoints_input.value))?maxSetPoints.Points:parseInt(maxPoints_input.value);
+        maxSetPoints.diffPoints = pDiff_input.checked;
+        maxSetPoints.firstReachMax = gPoint_input.checked;
         closeModal();
         renderGame();
     });
+
+    formEnd.appendChild(cancelButton);
+    formEnd.appendChild(saveButton);
+
+    let form = document.createElement("form");
+    form = appendChilds(form,[
+        maxPoints_label,
+        pDiff_label,
+        gPoint_label,
+        formEnd
+    ]);
+    setModalContent(form);
 }
 
+function setAttributes(element,attributes){
+    attributes.forEach((attribute)=>{
+        element.setAttribute(attribute[0],attribute[1])
+    });
+    return element;
+}
 function setModalContent(content){
-    modalContent.innerHTML = content;
+    modalContent.innerHTML = "";
+    modalContent.appendChild(content);
 }
-
+function appendChilds(element, childs){
+    childs.forEach((child)=>{
+        element.appendChild(child);
+    });
+    return element;
+}
 function winCondition(){
     if(maxSetPoints.firstReachMax){
         if(TeamA.Score==maxSetPoints.Points || TeamB.Score==maxSetPoints.Points){
@@ -400,7 +456,7 @@ function winCondition(){
             }
         }
     }else{
-        if(Math.abs(TeamA.Score-TeamB.Score)==2 && (TeamA.Score >= maxSetPoints.Points || TeamB.Score >= maxSetPoints.Points)){
+        if(Math.abs(TeamA.Score-TeamB.Score)>=2 && (TeamA.Score >= maxSetPoints.Points || TeamB.Score >= maxSetPoints.Points)){
             if(TeamA.Score>TeamB.Score){
                 winTeam('A');
             }else{
